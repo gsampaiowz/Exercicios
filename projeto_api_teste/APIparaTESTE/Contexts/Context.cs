8ab716f5b7c01f3883ad1591295da67ffcf9d056
gsampaiowz <gabrielsampaio1216@gmail.com>
2024-08-07 11:34:34 -0300

    Iniciando testes da API

---
+++ projeto_api_teste/APIparaTESTE/Contexts/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APIparaTESTE.Domains;
+
+namespace APIparaTESTE.Contexts
+    {
+    public class Context : DbContext
+        {
+        public DbSet<Product> Produto { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+            //optionsBuilder.UseSqlServer("Server = NOTE11-SALA21; Database = BancoTesteTarde; User Id = sa; Password = Senai@134; TrustServerCertificate = True;");
+            optionsBuilder.UseSqlServer("Server = DESKTOP-G6K3C1V; Database = BancoTesteTarde; User Id = sampaio; Password = 2605; TrustServerCertificate = True;");
+            base.OnConfiguring(optionsBuilder);
+            }
+        }
+    }
